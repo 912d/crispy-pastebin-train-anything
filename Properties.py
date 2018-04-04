ed0c0c48a6e6bff5a236f0dbd3be556bc2efbb49
Jacek Musiał <github@musi.al>
2018-04-04 19:49:05 +0200

    Add properties.py managing our sensitive data by proxy

---
+++ Properties.py
@@ -0,0 +1,9 @@
+import config
+
+class Properties():
+    host = config.host
+    user = config.user
+    password = config.passwd
+    database = config.db
+    table = config.table
+    charset = config.charset
