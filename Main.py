835da67e40c168eec197fac6fcaa79a1f8c8a319
Jacek Musiał <github@musi.al>
2018-04-08 19:24:16 +0200

    Add logger usage

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
-#!/user/bin/python2.7
+# -- coding: utf-8 --
 import urllib2
 import MySQLdb
 import json
 import time
+import Logger
+from Properties import Properties
 
 
 class PastebinScraper():
@@ -11,30 +13,29 @@ class PastebinScraper():
     cursor = None
     conn = None
 
-    def __init__(self):
-
-
     def getPasteText(self, paste_key):
         try:
             req = urllib2.urlopen('https://pastebin.com/raw/' + paste_key, timeout=3)
             pasteText = req.read()
-            length = len(pasteText)
-            file = open('p2/'+paste_key, "w")
+            file = open(Properties.pastedirectory + paste_key, "w")
             file.write(pasteText)
             file.close()
             return pasteText
         except IOError as e:
+            Logger.log("getPasteText", e)
             print("[!] API Error1:", e)
 
+
     def initDB(self):
+        Logger.log("initDB", "Initializing database..")
         try:
             self.conn = MySQLdb.connect(
-                host="localhost",
-                user="root",
-                passwd="nanomader#!$!%(",
-                db="pastebin",
+                host=Properties.host,
+                user=Properties.user,
+                passwd=Properties.password,
+                db=Properties.database,
                 use_unicode=True,
-                charset="utf8mb4"
+                charset=Properties.charset
             )
             self.conn.set_character_set('utf8')
             self.cursor = self.conn.cursor()
@@ -42,11 +43,13 @@ class PastebinScraper():
             self.cursor.execute('SET CHARACTER SET utf8mb4;')
             self.cursor.execute('SET character_set_connection=utf8mb4;')
         except Exception as e:
-            print("[!] API Error2:", e)
+            Logger.log("initDB", e)
+            print("[!] API Error:", e)
+
 
     def scraper(self):
         data, limit = self.getlimiteddata()
-        print('Starting looping through pastes')
+        Logger.log("scraper", "Starting looping through pastes")
 
         for d in range(limit):
             print("date: " + data[d]["date"])
@@ -56,8 +59,8 @@ class PastebinScraper():
                  VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)""", (data[d]["full_url"], data[d]["date"], data[d]["key"], data[d]["size"], data[d]["expire"], data[d]["title"], data[d]["syntax"], data[d]["user"], txt))
                 self.conn.commit()
             except Exception as e:
+                Logger.log("scraper", e)
                 print("[!] API Error3:", e)
-
 
 
     def getlimiteddata(self):
@@ -65,22 +68,22 @@ class PastebinScraper():
         fetchurl = 'https://pastebin.com/api_scraping.php?limit=' + str(limit)
         req = urllib2.urlopen(fetchurl, timeout=3)
         data = json.loads(req.read())
-        print('data length: ' + str(len(data)))
-        self.client.captureMessage('data length: ' + str(len(data)))
         return data, limit
 
 
 if __name__ == "__main__":
+    Logger.log("main", "Starting pastebin scrapper")
     paste = PastebinScraper()
     paste.initDB()
+
     while True:
         try:
             start = time.time()
             paste.scraper()
             end = time.time()
-            secsleft = int(round(end - start))
-            paste.client.captureMessage('Sleeping for ' + str(secsleft))
-            time.sleep(secsleft - 5)
+            secsleft = 60 - (int(round(end - start)))
+            Logger.log("main", str(secsleft) + " seconds sleeping")
+            time.sleep(secsleft)
         except Exception as e:
+            Logger.log("main", e)
             print("[!] API Error4:", e)
-            paste.client.captureException()
