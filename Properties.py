725608f0f0760b66cdeb34936d66704ccbfaf837
Jacek Musiał <github@musi.al>
2018-04-08 19:24:24 +0200

    Add two fields to the properties

--- Properties.py
+++ Properties.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 import config
 
 class Properties():
@@ -7,3 +8,5 @@ class Properties():
     database = config.db
     table = config.table
     charset = config.charset
+    restlogger = config.restlogger
+    pastedirectory = config.pastedirectory
