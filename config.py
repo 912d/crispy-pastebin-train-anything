5dadb7044cce3d1792236639d3ea17940c58dc27
Jack Musial <github@musi.al>
2018-04-15 10:05:02 +0200

    Create config.py
    Fill this file with your values

---
+++ config.py
@@ -0,0 +1,18 @@
+# -- coding: utf-8 --
+
+###database
+host=""
+user=""
+passwd=""
+db="pastebin"
+
+#recommended charset to catch properly every char, including Japanese, Polish, Emoji, etc.
+charset="utf8mb4"
+table="P2"
+
+###logger (In this project I use https://github.com/912d/restlogging)
+restlogger="http://localhost:8080"
+
+###directory
+# this path is used to, optionally, save pastes to file on your disk, besides saving it to the database
+pastedirectory=""
